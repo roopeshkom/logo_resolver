9ba09f86f77cb2cbbcde7c97b413a3bab3230c27
Roopeswar Kommalapati <rk650@kil017.nb-labs.rutgers.edu>
2018-05-07 18:12:48 -0400

    Resolved logo address from command line arg of website url

--- get_logo.py
+++ get_logo.py
@@ -1,6 +1,12 @@
 from __future__ import print_function
 import re
+from sys import argv
+import urllib
 
-with open('test', 'r+') as file:
-    reversed_text = str(file.read())[::-1]
-    print(re.search(r'("oci\..*?")', reversed_text).group(0)[::-1])
+file = urllib.urlopen(argv[1])
+reversed_text = file.read()[::-1]
+
+search_result = re.search(r'("oci\..*?")', reversed_text)
+ico_url = search_result.group(0)[::-1] if search_result else 'Could not resolve a logo...'
+
+print(ico_url)
